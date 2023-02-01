47207a245a26c380c4f648742ea3e41124913701
mtavano <chelotavano.udp@gmail.com>
2023-02-01 20:19:19 -0300

    Update linkTo prop to redirect properly

--- app/routes/admin/nodes.tsx
+++ app/routes/admin/nodes.tsx
@@ -22,7 +22,7 @@ export default function App() {
 
   return (
     <>
-      <HeaderDashboard title={"Nodes"} linkTo={"/nodes"}/>
+      <HeaderDashboard title={"Nodes"} linkTo={"/nodes/create/network"}/>
       
       <Outlet />
 

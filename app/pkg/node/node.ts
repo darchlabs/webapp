e877eeccf932a38d6d77c8a5ebaa67edeb004133
Nico <71539596+nicobarra@users.noreply.github.com>
2023-02-17 22:42:40 -0300

    fix(node-table): added delete buttons correctly with their interaction with the api. (#23)
    * (components/nodele/node-item.tsx): added delete item for deleting using the variable state nodeId with its handler function, and passing the value to the form
    
    * (components/node-table/node0item.tsx): added nodeId and hanlderNodeId as NodeTable params and passed by args to the node item
    
    * (pkg/node/node.ts): added DeleteNode methods using the api
    
    * (routes/admin/nodes.tsx): created id state variable w its handle
    r, inserted to the Table item as args. Put the table inside a form
    , and created action function for getting the action and executing
     the api

--- app/pkg/node/node.ts
+++ app/pkg/node/node.ts
@@ -1,51 +1,74 @@
 import fetch from "@remix-run/web-fetch";
 import type { GetNodesStatusResponse, PostNewNodeResponse } from "./requests";
+import type { HTTPResponse } from "../jobs/requests";
 
 export default class Node {
-    private URL: string;
+  private URL: string;
 
-    constructor(URL: string) {
-        this.URL = URL;
+  constructor(URL: string) {
+    this.URL = URL;
+  }
+
+  public getURL(): string {
+    return this.URL;
+  }
+
+  public async PostNewNode(
+    network: string,
+    fromBlockNumber: number
+  ): Promise<PostNewNodeResponse> {
+    try {
+      const url = `${this.URL}/api/v1/nodes`;
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ network, fromBlockNumber }),
+      });
+
+      const data = (await res.json()) as PostNewNodeResponse;
+      return data;
+    } catch (err: any) {
+      console.log(err);
+      throw err;
     }
+  }
 
-    public getURL(): string {
-        return this.URL;
+  public async GetStatus(): Promise<GetNodesStatusResponse> {
+    try {
+      const url = `${this.URL}/api/v1/nodes/status`;
+      const res = await fetch(url, {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+      const data = (await res.json()) as GetNodesStatusResponse;
+      return data;
+    } catch (err: any) {
+      console.log(err);
+      throw err;
     }
+  }
 
-    public async PostNewNode(network: string, fromBlockNumber: number): Promise<PostNewNodeResponse> {
-        try {
-            const url = `${this.URL}/api/v1/nodes`;
-            const res = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "content-type": "application/json",
-                },
-                body: JSON.stringify({network, fromBlockNumber}),
-            });
+  public async DeleteNode(id: string): Promise<HTTPResponse> {
+    try {
+      const url = `${this.URL}/api/v1/nodes`;
+      const res = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ nodeId: id }),
+      });
 
-            const data = (await res.json()) as PostNewNodeResponse;
-            return data;
-        } catch (err: any) {
-            console.log(err)
-            throw err;
-        }
+      const data = (await res.json()) as HTTPResponse;
+      return data;
+    } catch (err: any) {
+      console.log(err);
+      throw err;
     }
-
-    public async GetStatus(): Promise<GetNodesStatusResponse> {
-        try {
-            const url = `${this.URL}/api/v1/nodes/status`;
-            const res = await fetch(url, {
-                method: "GET",
-                headers: {
-                    "content-type": "application/json",
-                },
-            });
-
-            const data = (await res.json()) as GetNodesStatusResponse;
-            return data;
-        } catch (err: any) {
-            console.log(err)
-            throw err;
-        }
-    }
+  }
 }
--- app/routes/admin/nodes.tsx
+++ app/routes/admin/nodes.tsx
@@ -2,11 +2,11 @@ import { HStack } from "@chakra-ui/react";
 import NodeTable from "../../components/node-table/table";
 import HeaderDashboard from "../../components/header/dashboard";
 
-import { Outlet, useLoaderData } from "@remix-run/react";
+import { Form, Outlet, useLoaderData } from "@remix-run/react";
 import type { ActionArgs, LoaderFunction } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { node } from "../../pkg/node/node.server";
+import { useState } from "react";
 
 export const loader: LoaderFunction = async () => {
   const response = await node.GetStatus();
@@ -17,17 +17,45 @@ export function ErrorBoundary({ error }: { error: Error }) {
   return <>Oops, there is an error in nodes section</>;
 }
 
+export const action = async ({ request }: ActionArgs) => {
+  const body = await request.formData();
+
+  const id = `${body.get("nodeId")}`;
+  console.log("id: ", id);
+  const action = body.get("updateStatus");
+  console.log("aciton : ", action);
+
+  if (action === "delete") {
+    const res = await node.DeleteNode(id);
+    console.log("res: ", res);
+  }
+
+  return null;
+};
+
 export default function App() {
-  const { response, nodesURL} = useLoaderData<typeof loader>();
+  const { response, nodesURL } = useLoaderData<typeof loader>();
+
+  let [id, setId] = useState("");
+  function handlerNodeId(id: string) {
+    setId(id);
+  }
 
   return (
     <>
-      <HeaderDashboard title={"Nodes"} linkTo={"/nodes/create/network"}/>
-      
+      <HeaderDashboard title={"Nodes"} linkTo={"/nodes/create/network"} />
+
       <Outlet />
 
       <HStack justifyContent={"center"} w={"full"} pt={"20px"}>
-        <NodeTable nodeList={response.nodes} nodesURL={nodesURL} />
+        <Form method="post">
+          <NodeTable
+            nodeList={response.nodes}
+            nodesURL={nodesURL}
+            nodeId={id}
+            handlerNodeId={handlerNodeId}
+          />
+        </Form>
       </HStack>
     </>
   );

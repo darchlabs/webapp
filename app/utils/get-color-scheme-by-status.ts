4c39692bd5ffe8e878ac897933e265d9c33ddbb8
Camilo Acuña <camilo.aku@gmail.com>
2023-03-26 05:42:15 +0700

    feat(util): move method to select schema color by status

---
+++ app/utils/get-color-scheme-by-status.ts
@@ -0,0 +1,14 @@
+export function GetColorSchemeByStatus(status: string): string {
+  switch (status) {
+    case "running":
+      return "green";
+    case "sync":
+      return "yellow";
+    case "error":
+    case "stopped":
+    case "stopping":
+      return "red";
+  }
+
+  return "gray";
+}

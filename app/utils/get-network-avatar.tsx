a3379133432faeba64289f7cc0e0da8ae5ee185d
Camilo Acuña <camilo.aku@gmail.com>
2023-06-11 13:51:53 +0700

    refactor(util): update getNetworkAvatar and validateClient

--- app/utils/get-network-avatar.tsx
+++ app/utils/get-network-avatar.tsx
@@ -1,17 +1,30 @@
 import type { Network } from "darchlabs";
 
-import { PolygonAvatarIcon, EthereumAvatarIcon, AvalancheAvatarIcon } from "@components/icon";
+import {
+  PolygonAvatarIcon,
+  EthereumAvatarIcon,
+  AvalancheAvatarIcon,
+  CeloAvatarIcon,
+  ChainlinkAvatarIcon,
+} from "@components/icon";
 
 export const GetNetworkAvatar = (network: Network, size: number = 12) => {
   switch (network) {
     case "ethereum":
     case "rinkeby":
     case "goerli":
+    case "sepolia":
       return { ...(<EthereumAvatarIcon boxSize={size} />) };
     case "polygon":
     case "mumbai":
       return { ...(<PolygonAvatarIcon boxSize={size} />) };
-    case "avalanche":
-      return { ...(<AvalancheAvatarIcon boxSize={size} />) };
+    // case "avalanche":
+    // return { ...(<AvalancheAvatarIcon boxSize={size} />) };
+    case "celo":
+    case "alfajores":
+      return { ...(<CeloAvatarIcon boxSize={size} />) };
+    case "chainlink":
+    case "chainlink_sepolia":
+      return { ...(<ChainlinkAvatarIcon boxSize={size} />) };
   }
 };

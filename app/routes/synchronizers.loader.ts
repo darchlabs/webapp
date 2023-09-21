844c0b6675d71a5d86d35f51e7134b5571e5f236
Camilo Acuña <camilo.aku@gmail.com>
2023-09-21 15:31:57 +0200

    refactor(routes): update sync route componentes

--- app/routes/synchronizers.loader.ts
+++ app/routes/synchronizers.loader.ts
@@ -3,17 +3,22 @@ import type { LoaderArgs, LoaderFunction } from "@remix-run/node";
 import type { ListSmartContractsResponse, Pagination } from "darchlabs";
 import { SmartContracts } from "@models/synchronizers/smartcontracts.server";
 import { withPagination } from "@middlewares/with-pagination";
+import { AuthData, withAuth } from "@middlewares/with-auth";
 
 export type SmartContractsLoaderData = {
   smartcontracts: ListSmartContractsResponse;
+  auth: AuthData;
 };
 
-export const SmartContractsLoader: LoaderFunction = withPagination(async ({ context }: LoaderArgs) => {
+export const SmartContractsLoader: LoaderFunction = withAuth(withPagination(async ({ context }: LoaderArgs) => {
   // get pagination context for middleware
   const pagination = context.pagination as Pagination | {};
 
   // get smart contracts
   const smartcontracts = await SmartContracts.listSmartContracts(pagination);
 
-  return json<SmartContractsLoaderData>({ smartcontracts });
-});
+  // get auth context from middleware
+  const auth = context.auth as AuthData;
+
+  return json({ smartcontracts, auth });
+}));
--- app/routes/synchronizers.tsx
+++ app/routes/synchronizers.tsx
@@ -1,5 +1,5 @@
-import { HStack } from "@chakra-ui/react";
-import { Outlet, useLoaderData, useLocation } from "@remix-run/react";
+import { HStack, Text, VStack } from "@chakra-ui/react";
+import { Outlet, isRouteErrorResponse, useLoaderData, useLocation, useRouteError } from "@remix-run/react";
 import { Table } from "@components/table";
 import { EmptyTable, TableItem } from "@components/synchronizers/smartcontracts";
 import { BaseLayout } from "@components/layouts";
@@ -7,7 +7,32 @@ import { BaseError } from "@errors/base";
 import { SmartContractsLoader, type SmartContractsLoaderData } from "./synchronizers.loader";
 
 export const loader = SmartContractsLoader;
-export const ErrorBoundary = BaseError;
+// export const ErrorBoundary = BaseError;
+
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+  const { pathname } = useLocation();
+  if (isRouteErrorResponse(error)) {
+    console.log(`App Error(isRouteErrorResponse) = ${error}`);
+  } else {
+    console.log(`App Error = ${error}`);
+  }
+
+  return <HStack w={"full"} h={"full"}>
+    <VStack>
+      <Text>
+        404. That's an error.
+      </Text>
+      <Text>
+        The requested URL {pathname} was not found on this server.
+      </Text>
+      <Text>
+        That's all we know.
+      </Text>
+    </VStack>
+  </HStack>;
+};
+
 
 export default function App() {
   const {
@@ -15,14 +40,16 @@ export default function App() {
       data,
       meta: { pagination },
     },
-  } = useLoaderData<SmartContractsLoaderData>();
+    auth,
+  } = useLoaderData() as SmartContractsLoaderData;
+
   const { pathname } = useLocation();
 
   const linkTo = "/synchronizers/create";
   const tableOptions = pathname.includes("/create") ? {} : { emptyTable: <EmptyTable createLink={linkTo} /> };
 
   return (
-    <BaseLayout title="Synchronizers" linkTo={linkTo}>
+    <BaseLayout title="Synchronizers" linkTo={linkTo} auth={auth}>
       <Outlet />
 
       <HStack justifyContent={"center"} w={"full"} pt={"20px"}>

c495b0e78d102b7caa9a162a7535f4411c501d19
Camilo Acuña <camilo.aku@gmail.com>
2023-09-21 15:35:28 +0200

    refactor(models): add backoffice model and add several chages in job, nodes and sync cookies

--- app/models/backoffice/backoffice-cookie.server.ts
+++ app/models/backoffice/backoffice-cookie.server.ts
@@ -1,14 +1,14 @@
 import invariant from "tiny-invariant";
 import { createFileSessionStorage, createCookie } from "@remix-run/node";
 
-const { COOKIE_SECRET } = process.env;
+const { COOKIE_SECRET} = process.env;
 invariant(typeof COOKIE_SECRET === "string", "COOKIE_SECRET env var not set");
 
-const sessionCookie = createCookie("jobSession", {
+const backofficeSession = createCookie("backofficeSession", {
   secrets: [COOKIE_SECRET!]
 });
 
 export const { getSession, commitSession, destroySession } = createFileSessionStorage({
   dir: "./sessions",
-  cookie: sessionCookie,
+  cookie: backofficeSession,
 });

123556afccb484281d831f3aae2171f50a8d72d7
Camilo Acuña <camilo.aku@gmail.com>
2023-09-21 15:32:41 +0200

    feat(route): add logout route component

---
+++ app/routes/logout.action.ts
@@ -0,0 +1,12 @@
+import { redirect, type ActionFunction } from "@remix-run/node";
+import { getSession, destroySession } from "@models/backoffice/backoffice-cookie.server";
+
+export const action: ActionFunction = async ({ request }: { request: Request }) => {
+  const session = await getSession(request.headers.get("Cookie"));
+  
+  return redirect("/login", {
+    headers: {
+      "Set-Cookie": await destroySession(session),
+    },
+  });
+};

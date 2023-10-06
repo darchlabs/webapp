f06d16c8b0ac01db7e80578742a60d95bedaf839
Camilo Acuña <camilo.aku@gmail.com>
2023-10-06 06:28:07 +0200

    refactor: add several changes in models

--- app/models/backoffice/backoffice-cookie.server.ts
+++ app/models/backoffice/backoffice-cookie.server.ts
@@ -1,14 +1,12 @@
 import invariant from "tiny-invariant";
-import { createFileSessionStorage, createCookie } from "@remix-run/node";
+import { createCookieSessionStorage } from "@remix-run/node";
 
-const { COOKIE_SECRET} = process.env;
+const { COOKIE_SECRET } = process.env;
 invariant(typeof COOKIE_SECRET === "string", "COOKIE_SECRET env var not set");
 
-const backofficeSession = createCookie("backofficeSession", {
-  secrets: [COOKIE_SECRET!]
-});
-
-export const { getSession, commitSession, destroySession } = createFileSessionStorage({
-  dir: "./sessions",
-  cookie: backofficeSession,
+export const { getSession, commitSession, destroySession } = createCookieSessionStorage({
+  cookie: {
+    name: "backofficeSession",
+    secrets: [COOKIE_SECRET!]
+  },
 });
